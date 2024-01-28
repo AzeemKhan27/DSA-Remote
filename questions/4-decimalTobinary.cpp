016e7e83bf6e042037200e4c49509a5638f7c36a
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-01-28 19:42:57 +0530

    decimal to binary value code in c++

---
+++ questions/4-decimalTobinary.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int findBinary(int val){
+     int rem, mul=1, ans = 0;
+     while(val>0){
+        rem = val%2;
+        val/=2;
+        ans= rem*mul+ans;
+        mul*=10;
+     }
+     return ans;
+}
+
+int main(){
+    int value;
+    cout<<"enter decimal value to find correct binary value from given decimal value :"<<endl;
+    cin>>value;
+
+    int binaryVal = findBinary(value);
+    cout<<"Binary value is = "<<binaryVal <<endl;
+
+    return 0;
+}
