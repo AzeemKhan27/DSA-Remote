9640369e1c7a48a6ed79972a58879f97920d999f
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-01-28 12:15:35 +0530

    move 2 directories in DSA_Remote

---
+++ questions/1-isPrime.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int num){
+   if(n<2)
+   return 0;
+
+   for(int i=2; i<=num; i++){
+    if(n%i==0)
+      return 0;
+   }
+   return 1;
+}
+
+int main(){
+   int num;
+   cout<<"enter the number :" endl;
+   cin>>num;
+   bool isPrime(num);
+   cout<<num;
+}
+
+git remote add origin https://github.com/AzeemKhan27/DSA-Remote.git
