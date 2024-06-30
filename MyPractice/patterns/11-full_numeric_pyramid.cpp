c1ac2de66b2c130543d84910949b0373a6f6d5aa
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-06-30 07:39:47 +0000

    Pattern : full numeric pyramid

---
+++ MyPractice/patterns/11-full_numeric_pyramid.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+void numeric_full_pyramid(int num) {
+     
+
+    for(int row=1; row<=num; row++){                 // Aproach-1
+
+        //Spaces
+
+        for(int col=1; col<=num-row; col++){
+            cout<<"   ";
+        }
+        
+        //Make numeric half pyramid.
+
+        // for(int col=2*row-row; col<=2*row-1; col++){  // both are working
+        for(int col=row; col<=2*row-1; col++){
+            cout<<col<<"  ";
+        }
+
+
+        //left numeric pyramid
+
+        for(int col=2*row-2; col>=row; col--){
+            cout<<col<<"  ";
+        }
+           cout<<endl;
+    }          
+   
+}
+
+int main() {
+    int num;
+    cout << "Enter the number of rows and columns to make a numeric full pyramid shape by stars: " << endl;
+    cin >> num; 
+
+    numeric_full_pyramid(num);
+    
+    return 0;
+}
+
+
+// //OUTPUT :
+//row = 9
+
+//   * * * * * * * *
+//    * * * * * * *
+//     * * * * * *
+//      * * * * *
+//       * * * *
+//        * * *
+//         * *
+//          *
