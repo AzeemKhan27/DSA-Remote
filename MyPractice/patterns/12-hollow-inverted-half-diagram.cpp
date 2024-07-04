c9ef21e7d54bb4c0f0c8007ef75b597dba95d828
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-07-04 13:39:47 +0000

    PATTERN : Hollow Full Pyramid

--- MyPractice/patterns/12-hollow-inverted-half-diagram.cpp
+++ MyPractice/patterns/12-hollow-inverted-half-diagram.cpp
@@ -4,20 +4,24 @@ using namespace std;
 void inverted_half_pyramid(int num) {
      
 
-    for(int row=0; row<num; row++){  
+    for(int row=1; row<=num; row++){  
 
-       
-                for(int col=0; col<num; col++){
-              
-                  if(row == 0 || col == 0 || col == num-row-1){
-                      cout<<"* ";
-                  }
-                  else{
-                    cout<<"  ";
-                  }
-                
-               }   
-        cout<<endl;
+       // spaces
+       for(int col=1; col <= num-row; col++){
+          cout<<"  ";
+       }
+  
+            
+       // stars
+       for(int col=1; col<=2*row-1; ++col){
+        if(col == 1 || col == 2*row-1 || row == num){
+            cout<<"* ";
+        }else{
+            cout<<"  ";
+        }
+       }
+       cout<<endl;
+
     }         
     
 }
