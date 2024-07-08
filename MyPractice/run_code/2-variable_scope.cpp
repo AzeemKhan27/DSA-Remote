9a70fb6a75e96a04409f018f06625c0286b5f775
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-07-08 08:25:25 +0000

    2-variable_scope

---
+++ MyPractice/run_code/2-variable_scope.cpp
@@ -0,0 +1,26 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    //declaration
+    int a;
+    //initialisation
+    int b = 5;
+    //updation
+    b=10;
+
+    if(true){
+        int b = 15;
+        cout<<b<<endl;
+    }
+    cout<<b<<endl;
+}
+
+
+// Output: 
+
+// first : 15;
+// Then : 10;
