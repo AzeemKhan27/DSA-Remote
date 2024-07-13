422e340574b8d5860f325a1671913986625400ed
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-07-13 16:39:52 +0000

    Revised Pattern : Numeric Paliindrom

---
+++ MyPractice/patterns/Revised_Pattern/8-numeric-palindrom.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void numeric_palindrom(int num) {
+    
+    for(int row=1; row<=num; row++){
+
+        for(int col=1; col<=num-row; col++){
+            cout<<"  ";
+        }
+
+        for(int col=1; col<=row; col++){
+            cout<<col<<" ";
+        }
+
+
+        for(int col=row-1; col>=1; col--){
+            cout<<col<<" ";
+        }
+
+        // for(int col=1; col<=row-1; col++){
+        //     cout<<col<<" ";
+        // }
+
+        cout<<endl;
+
+        // for(int)
+       
+    }
+}
+
+int main() {
+    int num;
+    cout << "Enter the number : " << endl;
+    // cin >> num; 
+
+    numeric_palindrom(6);
+
+    return 0;
+}
