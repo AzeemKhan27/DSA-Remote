f65c9d8ce277715921c1853ddd8a6106bebb23ab
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-07-15 12:05:28 +0000

    Revised Pattern : Aproach-2 Solid Half Diamond

--- MyPractice/patterns/Revised_Pattern/9-solid_half_diamond.cpp
+++ MyPractice/patterns/Revised_Pattern/9-solid_half_diamond.cpp
@@ -1,22 +1,36 @@
 #include<iostream>
 using namespace std;
 
-void solid_half_diamond(int num) {
+// void solid_half_diamond(int num) {
     
-    for(int row=1; row<=num; row++){
+//     for(int row=1; row<=num; row++){
 
-        for(int col=1; col<=row; col++){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
+//         for(int col=1; col<=row; col++){
+//             cout<<"*"<<" ";
+//         }
+//         cout<<endl;
        
-    }
+//     }
 
-    for(int i=1; i<num; i++){
-        for(int j=1; j<=num-i; j++){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
+//     for(int i=1; i<num; i++){
+//         for(int j=1; j<=num-i; j++){
+//             cout<<"*"<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+void solid_half_diamond(int n) {
+    
+    for(int row=1; row<=2*n-1; row++){
+           
+          double condition = row <= n ? row : n - (row%n) ;
+
+          for(int col=1; col<=condition; col++){
+               cout<<"*";
+          }
+
+          cout<<endl;
     }
 }
 
@@ -24,7 +38,7 @@ int main() {
     int num;
     cout << "Enter the number : " << endl;
     // cin >> num; 
-    solid_half_diamond(6);
+    solid_half_diamond(5);
 
     return 0;
 }
