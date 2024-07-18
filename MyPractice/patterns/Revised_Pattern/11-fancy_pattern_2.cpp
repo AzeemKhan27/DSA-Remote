50d34cb19dbed5fe2c079423162bc9abdf09de59
Azeem Khan <121457811+AzeemKhan27@users.noreply.github.com>
2024-07-18 06:37:54 +0000

    Pattern: Fancy Fantern:  add 1 star in the top row/col and last row/col also Growing Face and Shrinking Face

--- MyPractice/patterns/Revised_Pattern/11-fancy_pattern_2.cpp
+++ MyPractice/patterns/Revised_Pattern/11-fancy_pattern_2.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 void fancy_pattern_2(int n) {
+
+     // print 1st row star
+           cout<<"*"<<endl;
     
     for(int i=0; i<n; i++){
         int cond = i <= n/2 ? 2*i : 2*(n-i-1);  // this condition for row wise growing face : i <= n/2 ,  row-wise-shirinking face thinking condition :  2*i : 2*(n-i-1)
+
+        // print stars in starting :
+
+            cout<<"*";
  
         for(int j=0; j<=cond; j++){
             if(j<=cond/2){
@@ -14,8 +21,16 @@ void fancy_pattern_2(int n) {
                 cout<<cond-j+1;
             }
         }
+
+        // print stars in ending:
+
+        cout<<"*";
+
         cout<<endl;
     }
+
+    // last row star
+    cout<<"*"<<endl;
    
 }
 
@@ -32,13 +47,15 @@ int main() {
 
 // n=10
 
-// 1
-// 121
-// 12321
-// 1234321
-// 123454321
-// 12345654321
-// 1234321
-// 12321
-// 121
-// 1
+// *
+// *1*
+// *121*
+// *12321*
+// *1234321*
+// *123454321*
+// *12345654321*
+// *1234321*
+// *12321*
+// *121*
+// *1*
+// *
