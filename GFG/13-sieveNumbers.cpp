e66bf13593c1a70a0d1c537a4a01f3b6fa6c22b0
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-08-02 11:36:36 +0530

    DSA: Sieve Number Code Issue Resolved

--- GFG/13-sieveNumbers.cpp
+++ GFG/13-sieveNumbers.cpp
@@ -33,14 +33,20 @@ bool isPrime(int n){
 // Efficient Solution:
 void printSieve(int n){
     vector<bool> isPrime(n+1, true);
-    for(int i=2; i<=n; i++){
-        if(isPrime(i))
-        cout<<i<<" ";
-
-    for(int j=2=i*i; j<=n; j=j+i){
-        isPrime[j] = false;
-    }
+    for(int i=2; i<=n; ++i){
+        if(isPrime[i])
+        {
+            for(int j=i*i; j<=n; j += i){
+            isPrime[j] = false;
+         }
+        }
   }    
+  for(int i=2; i<=n; ++i){
+    if(isPrime[i]){
+        cout<<i<<" ";
+    }
+  }
+  cout<<endl;
 }
 
 int main() {
