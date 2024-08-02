40ae459edcd20493410b6df13fce1f9bd2958eb5
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-08-02 17:58:43 +0530

    DSA: Iterative power

---
+++ GFG/14-iterativePower.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+// Solution:
+int power(int x, int n){
+    int res = 1;
+    
+    while(n>0){
+        if(n%2==1){
+        res *= x;
+     }
+
+     x*=x;
+     n/=2;
+    }
+    return res;
+}
+
+// Efficient Solution:
+
+
+int main() {
+        int num,pow;
+        cout<<"Enter a num : "<<endl;
+        cin>>num>>pow;
+    	int result = power(num,pow);
+        cout<<"Result : "<<result<<endl;
+    	return 0;
+}
