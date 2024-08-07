a29b848f2f4fa6dacda9b2a0533c06c17c4e2fcc
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-08-07 19:02:39 +0530

    DSA/Array : Remove Duplicate

---
+++ GFG/array/5-duplicate.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int removeDuplicate(int arr[], int n){
+   int res = 1;
+   for(int i = 0; i<n; i++){
+    if(arr[i] != arr[res-1]){
+        arr[res] = arr[i];
+        res+=1;
+    }
+   }
+   return res;
+}
+
+int main(){
+//    int arr[] = {10,20,20,40,50,30,40,50,30,10,10,20,30,40,50,60,50,30,40};
+   int arr[] = {10,10,20,20,30,30,40,40,50,50};
+   int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Before Removal"<<endl;
+
+   for(int i=0; i<n; i++){
+     cout << arr[i] << " ";
+   }
+
+   cout << endl;
+
+   n = removeDuplicate(arr,n);
+
+   cout<<"After Reverse:"<<endl;
+
+   for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+   }
+}
