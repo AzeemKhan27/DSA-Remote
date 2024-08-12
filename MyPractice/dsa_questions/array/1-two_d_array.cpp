c07cd844ea62871fb9403e8931d0dd5873bdcc49
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-08-12 12:48:09 +0530

    DSA/Array: 2dArray Vector

--- MyPractice/dsa_questions/array/1-two_d_array.cpp
+++ MyPractice/dsa_questions/array/1-two_d_array.cpp
@@ -4,10 +4,7 @@
 using namespace std;
 
 int main(){
-    int row=5;
-    int col=5;
-
-    vector<vector<int> > arr2 (row,vector<int>(col,0));
+    vector<vector<int> > arr (5,vector<int>(5,-8));
 
     for(int i=0; i<arr.size(); i++){
         for(int j=0; j<arr[i].size(); j++){
