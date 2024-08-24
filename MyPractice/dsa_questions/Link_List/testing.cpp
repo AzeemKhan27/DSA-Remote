ddd3edeb63c0ef0928fe7072e6012d3d66c06c35
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-08-24 18:52:20 +0530

    testing

---
+++ MyPractice/dsa_questions/Link_List/testing.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node* next;
+
+    Node(){
+        int->data = 0;
+        next = nullptr;
+    }
+
+    Node(int data){
+        this->data = data;
+        next = nullptr;
+    }
+};
+
+int main(){
+
+    Node* first = new Node;
+    Node* second = new Node;
+    Node* third = new Node;
+    Node* fourth = new Node();
+    Node* fifth = new Node();
+
+    first->next = second;
+    
+    
+
+    first->data
+}
+
+
+
+int main(){
+    Node* node = new Node();
+
+    return 0;
+}
