a38c7bb8a167840f67d1d46a90ed708d6f5830d4
AzeemKhan27git config --global user.name AzeemKhan27git config --global user.name AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-09-15 18:09:34 +0530

    DSA/GFG/Live : Graph : find town judge

---
+++ GFG/LIVE/Graph/1-find_town_judge.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+#include<vector>
+
+using namespace std;
+
+int main(){
+    int numberOfNodes,numberOfEdges;
+
+    cin>>numberOfNodes>>numberOfEdges;
+
+    vector<int> G[numberOfNodes+1];
+
+    for(int i=0;i<numberOfEdges;i++){
+
+        int x,y;
+
+        cin>>x>>y;
+
+        G[x].push_back(y);
+
+        G[y].push_back(x);
+
+    }
+
+ 
+
+    // Print the elements
+
+    for (int i = 1; i <=numberOfNodes; ++i) {
+
+        cout << "[" << i << "]: ";
+
+        for (const auto& elem : G[i]) {
+
+            cout << elem << " ";
+
+        }
+
+        cout << endl;
+
+    }
+
+ 
+
+    return 0;
+}
