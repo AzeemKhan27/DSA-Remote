8de4aa726ec5045df7b988ccc2f31400de96bf14
AzeemKhan27git config --global user.name AzeemKhan27git config --global user.name AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-09-16 17:57:05 +0530

    HackerRank/Easy : Absract Class

---
+++ hackerRank/easy/3-Abstract_Classes.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Book{
+    public: 
+        virtual void display()=0;
+};
+
+class MyBook : public Book{
+    private:
+        string title;
+        string author;
+        int price;
+    
+    public:
+        //parameterized constructor
+        MyBook(string t, string a, int p) : title(t), author(a), price(p) {}
+        
+    //imple the pure virtual function
+    void display(){
+        cout<<"Title: "<<title <<endl;
+        cout<<"Author: "<<author <<endl;
+        cout<<"Price: "<<price <<endl;
+    }
+};
+
+int main() {
+    int price;
+    string title,author;
+    
+    //Assume input
+    title = "The Alchemist";
+    author = "Paulo Coelho";
+    price = 248;
+    
+    
+    MyBook myBook(title, author, price);
+    myBook.display(); 
+    return 0;
+}
