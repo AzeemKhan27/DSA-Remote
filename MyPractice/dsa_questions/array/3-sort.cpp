2e5c6a19f7eeeccbc4610ff75938d66fdac98d14
AzeemKhan27git config --global user.name AzeemKhan27git config --global user.name AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-10-03 18:15:44 +0530

    Revised : Sort an array

--- MyPractice/dsa_questions/array/3-sort.cpp
+++ MyPractice/dsa_questions/array/3-sort.cpp
@@ -1,29 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void sortArray(int *arr, int n){
-
-    int i=0;
-    int index = arr[i] - 1;
+void sortArray(int *arr, int n) {
+    int i = 0;
     while (i < n) {
-      if(arr[i] != arr[index]){
-        swap(arr[i], arr[index + 1]);
-      }else{
-        ++i;
-      }
+        int correct_pos = arr[i] - 1;
+        if (correct_pos >= 0 && correct_pos < n && arr[i] != arr[correct_pos]) {
+            swap(arr[i], arr[correct_pos]);
+        } else {
+            ++i;
+        }
     }
 
-     for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-int main(){
-    int arr[] = {1, 2, -3, 4, -5, 6};
+int main() {
+    int arr[] = {1, 2, -3, 4, -5, 9, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
-
     sortArray(arr, n);
-    
     return 0;
 }
