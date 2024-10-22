58bd886a1ec557e40731d35f79f27d2f87645a63
AzeemKhan27 <azeemdeveloper27@gmail.com>
2024-10-22 18:50:28 +0530

    practice: isprime

--- MyPractice/1-isPrime.cpp
+++ MyPractice/1-isPrime.cpp
@@ -21,3 +21,27 @@ int main(){
 
    return 0;
 }
+
+// pratice 
+
+bool isPrime(int num){
+if(num <2)
+return 0;
+
+for(int i=2; in<=num; i++){
+if(num%i==0){
+     return 0;
+  }
+return 1;
+}
+
+int main(){
+	int num;
+cout<<"enter the number : " <<endl;
+cin>> num;
+bool>>num;
+bool isPrime(num);
+cout<<num;
+
+return 0;
+}
